c3071402a02093e19b46ba11e3a951e5396d9083
omarfandino <fandino.omar@gmail.com>
2021-09-15 18:48:43 -0500

    Updated CharactersListScreen to show characters with their respective house

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -1,5 +1,7 @@
 import { API_URL } from '../config/envVariables';
 
 export const booksEndpoint = `${API_URL}books/all`;
-
 export const bookDetailEndpoint = (id: number) => `${API_URL}books/${id}`;
+
+export const charactersEndpoint = `${API_URL}characters/all`;
+export const characterDetailEndpoint = (id: number) => `${API_URL}characters/${id}`;
